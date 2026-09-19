0546da4da905790f3e8cc7e23ff93ea514230bc5
SolarAI Capital <160229357+CryptolordUg@users.noreply.github.com>
2026-09-19 03:28:05 -0400

    Create layout.tsx

---
+++ app/layout.tsx
@@ -0,0 +1,1 @@
+export default function RootLayout({children}:{children:any}){return <html><body style={{margin:0,background:"#020617",color:"#fff",fontFamily:"system-ui"}}>{children}</body></html>}
