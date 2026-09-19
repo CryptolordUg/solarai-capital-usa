ce0142e3b223122b2407bc1e94041da64345a884
SolarAI Capital <160229357+CryptolordUg@users.noreply.github.com>
2026-09-19 09:17:27 -0400

    Delete layout.tsx

--- layout.tsx
+++
@@ -1,8 +0,0 @@
-export default function Home() {
-  return (
-    <main style={{ padding: '50px', textAlign: 'center' }}>
-      <h1>SolarAI Capital USA</h1>
-      <p>Site is now working - CryptolordUg</p>
-    </main>
-  );
-}
