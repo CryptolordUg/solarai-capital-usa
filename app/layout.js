26e9972467d9e7ebbb9f590d9f304bde9f03536a
SolarAI Capital <160229357+CryptolordUg@users.noreply.github.com>
2026-09-20 00:55:06 -0400

    Create layout.js

---
+++ app/layout.js
@@ -0,0 +1,5 @@
+import "./globals.css";
+export const metadata = { title: "SolarAI Capital USA", description: "Solar Investment Platform" };
+export default function RootLayout({ children }) {
+  return (<html lang="en"><body>{children}</body></html>);
+}
